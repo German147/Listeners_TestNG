137610724d80bae6adafdb45a8d1fecfdf07c087
kdenysiuk <103523831+kdenysiuk@users.noreply.github.com>
2022-10-03 16:06:50 -0300

    Revert "Melina ok"

--- src/main/java/com/solvd/testing/zebrunner/api/AuthToken.java
+++ src/main/java/com/solvd/testing/zebrunner/api/AuthToken.java
@@ -5,24 +5,29 @@ This class is implemented as a singleton, since we only need one instance of the
 REST API call. The token is automagically refreshed once it expires, so no need of checking dates.
 */
 
-public class AuthToken {
+public class AuthToken
+{
     private static AuthToken instance;
+
     private String authToken;
 
-    private AuthToken() {
-    }
-
-    public static synchronized AuthToken getInstance() {
-        if (instance == null) {
-            //Read the auth details from the property file
-            instance = new AuthToken();
-            instance.authToken = RestApiWrapper.getAuthToken();
-        }
-        return instance;
-    }
+    private AuthToken(){}
 
     public String getAuthToken() {
         return authToken;
+    }
+
+    public static synchronized AuthToken getInstance(){
+        if(instance == null){
+            //Read the auth details from the property file
+
+
+            instance = new AuthToken();
+
+            instance.authToken = RestApiWrapper.getAuthToken();
+
+        }
+        return instance;
     }
 
 }
