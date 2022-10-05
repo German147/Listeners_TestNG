6736a96e982cd291752ba99aec3c09e9ec556018
German <barreragerman27@gmail.com>
2022-10-05 15:33:35 -0300

    ScreenShots

--- src/main/java/com/solvd/testing/pages/HomePage.java
+++ src/main/java/com/solvd/testing/pages/HomePage.java
@@ -1,7 +1,13 @@
 package com.solvd.testing.pages;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
 
 public class HomePage extends BasePage{
     public HomePage(WebDriver driver) {
@@ -11,6 +17,13 @@ public class HomePage extends BasePage{
 
     public SidePanelPage clickOnBurgerButton(){
       driver.findElement(burgerButton).click();
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File file = ts.getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(file,new File("./screenshots/burgerButtonClick.jpg"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         return new SidePanelPage(driver);
     }
 }
--- src/main/java/com/solvd/testing/pages/SidePanelPage.java
+++ src/main/java/com/solvd/testing/pages/SidePanelPage.java
@@ -1,10 +1,15 @@
 package com.solvd.testing.pages;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public class SidePanelPage extends BasePage {
@@ -19,12 +24,26 @@ public class SidePanelPage extends BasePage {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         driver.findElement(platFormLink).click();
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(zebRunnerDeviceLink)));
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File file = ts.getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(file,new File("./screenshots/Image1.jpg"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public ZebrunnerDeviceFarmPage clickOnZebrunnerDevicePage() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         driver.findElement(zebRunnerDeviceLink).click();
         wait.until(ExpectedConditions.invisibilityOf(driver.findElement(zebRunnerDeviceLink)));
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File file = ts.getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(file,new File("./screenshots/OpenedDevice.jpg"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         return new ZebrunnerDeviceFarmPage(driver);
     }
 
